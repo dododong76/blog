0af9e7b690e998ae774715d5697cc7b825ca6117
dododong76 <dododong76@gmail.com>
2021-05-07 14:21:12 +0900

    'two'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [a, b] = useState(["김동은", "김승민"]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{a[0]} 이에요</p>
+        <p>안녕하세요!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React.
         </a>
+        <h2>
+          열심히 노력합시다. 또 노력합시다.
+        </h2>
+
+
       </header>
     </div>
   );
+
 }
 
 export default App;
